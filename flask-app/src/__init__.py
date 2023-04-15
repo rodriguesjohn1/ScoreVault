de1499b9ccf231f92d3518fd9e8d86bca743715f
John Rodrigues <rodrigues.joh@northeastern.edu>
2023-04-15 12:09:04 -0400

    Edit init to handle new blueprints

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -1,4 +1,4 @@
-# Some set up for the application 
+# Some set up for the application
 
 from flask import Flask
 from flaskext.mysql import MySQL
@@ -6,24 +6,27 @@ from flaskext.mysql import MySQL
 # create a MySQL object that we will use in other parts of the API
 db = MySQL()
 
+
 def create_app():
     app = Flask(__name__)
-    
-    # secret key that will be used for securely signing the session 
-    # cookie and can be used for any other security related needs by 
+
+    # secret key that will be used for securely signing the session
+    # cookie and can be used for any other security related needs by
     # extensions or your application
     app.config['SECRET_KEY'] = 'someCrazyS3cR3T!Key.!'
 
-    # these are for the DB object to be able to connect to MySQL. 
-    app.config['MYSQL_DATABASE_USER'] = 'root'
-    app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
+    # these are for the DB object to be able to connect to MySQL.
+    app.config['MYSQL_DATABASE_USER'] = 'webapp'
+    app.config['MYSQL_DATABASE_PASSWORD'] = open(
+        '/secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'northwind'  # Change this to your DB name
+    # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'ScoreVaultDB'
 
-    # Initialize the database object with the settings above. 
+    # Initialize the database object with the settings above.
     db.init_app(app)
-    
+
     # Add a default route
     @app.route("/")
     def welcome():
@@ -31,12 +34,12 @@ def create_app():
 
     # Import the various routes
     from src.views import views
-    from src.customers.customers import customers
-    from src.products.products  import products
+    from src.analysts.analysts import analysts
+    from src.executives.executives import executives
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(analysts,   url_prefix='/a')
+    app.register_blueprint(executives,    url_prefix='/e')
 
     return app
